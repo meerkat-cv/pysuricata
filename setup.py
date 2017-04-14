7a59ef6127c180d32dbba8575c85039ae561d26c
Gustavo Führ <gustavofuhr87@gmail.com>
2017-04-14 01:23:14 -0300

    Set setup.py for pip installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pysuricata',
+      version='0.1',
+      description='Meerkat\'s package of Computer Vision utilities',
+      url='https://github.com/meerkat-cv/pysuricata',
+      author='Meerkat',
+      author_email='support@meerkat.com.br',
+      license='MIT',
+      packages=['pysuricata.video_stream'],
+      zip_safe=False)
