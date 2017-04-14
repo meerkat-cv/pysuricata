9a9d02bb3b818ebecdcc9b78ca06e255c2a19eeb
Gustavo Führ <gustavofuhr87@gmail.com>
2017-04-14 01:37:34 -0300

    Add two simple annotation scripts

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='pysuricata',
-      version='0.1',
+      version='0.2',
       description='Meerkat\'s package of Computer Vision utilities',
       url='https://github.com/meerkat-cv/pysuricata',
       author='Meerkat',
       author_email='support@meerkat.com.br',
       license='MIT',
       packages=['pysuricata.video_stream'],
+      scripts=['scripts/anno_tools/keep_or_gone', 'scripts/anno_tools/yes_no_reject'],
       zip_safe=False)
