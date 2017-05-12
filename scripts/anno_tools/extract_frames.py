bac6b06ab5fc0867ea02448378a0c5f097658ec1
Guilherme Fickel <guilhermefickel@gmail.com>
2017-05-12 15:07:51 -0300

    Adding script to extract frames from webcam

---
+++ scripts/anno_tools/extract_frames.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+ret, frame = cap.read()
+
+frame_num = 0
+while frame is not None:
+    frame_num += 1
+    if frame_num % 30 == 0:
+        cv2.imwrite('frame_'+str(frame_num/30).zfill(3)+'.png', frame)
+    cv2.imshow('Cam', frame)
+    cv2.waitKey(1)
+
+    ret, frame = cap.read()
